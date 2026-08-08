4401f642f22090ca2487973ba74474d6a4f66d97
Dominik Galus <dominikgalus9@gmail.com>
2026-08-08 02:45:09 +0200

    feat: connection pool settings and alembic

--- services/auth-service/migrations/env.py
+++ services/auth-service/migrations/env.py
@@ -3,18 +3,20 @@ import asyncio
 from alembic import context
 from common.db import Base
 from common.settings import CommonSettings
-from sqlalchemy.ext.asyncio import async_engine_from_config
+from sqlalchemy import pool
+from sqlalchemy.ext.asyncio import create_async_engine
 
-config = context.config
-
-config.set_main_option("sqlalchemy.url", CommonSettings().database_url)
+DATABASE_URL = CommonSettings().database_url
+if not DATABASE_URL:
+    msg: str = "DATABASE_URL is not set"
+    raise RuntimeError(msg)
 
 target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
     context.configure(
-        url=config.get_main_option("sqlalchemy.url"),
+        url=DATABASE_URL,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -35,10 +37,7 @@ def _do_run_migrations(connection) -> None:
 
 
 async def run_migrations_online() -> None:
-    engine = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-    )
+    engine = create_async_engine(DATABASE_URL, poolclass=pool.NullPool)
     async with engine.connect() as connection:
         await connection.run_sync(_do_run_migrations)
     await engine.dispose()
